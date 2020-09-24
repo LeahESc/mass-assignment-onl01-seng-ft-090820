51e1bdf3159f7885f7f19ee52579a4c949da31df
Leah Schlackman <leahemily1@gmail.com>
2020-09-24 17:46:55 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,5 +1,5 @@
 class Person
-  attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_size, :pant_width 
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_size, :pant_width, :pant_length
   
   def initialize(attributes)
     attributes.each {|key, value| self.send(("#{key}="), value)}
